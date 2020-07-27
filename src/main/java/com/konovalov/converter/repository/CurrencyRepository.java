af860d704bfc9d800d0ceadd0e390a90ad071c4c
MS.Konovalov <konovalov.ms93@gmail.com>
2020-07-27 12:38:00 +0300

    Unused method removing

--- src/main/java/com/konovalov/converter/repository/CurrencyRepository.java
+++ src/main/java/com/konovalov/converter/repository/CurrencyRepository.java
@@ -3,17 +3,12 @@ package com.konovalov.converter.repository;
 import com.konovalov.converter.entity.Currency;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.util.Date;
 import java.util.List;
 
 @Repository
 public interface CurrencyRepository extends JpaRepository<Currency, String> {
-    @Query("select r.currency from Rate r where r.date = :rateDate order by r.currency.charCode")
-    List<Currency> findCurrenciesForDate(@Param("rateDate") Date rateDate);
-
     @Query("select r.currency from Rate r where r.date = (select max(date) from Rate) order by r.currency.charCode")
     List<Currency> findCurrenciesWithRelevantRates();
 }
